0b33e5f2c71a5e9b8ab9f39b86573394ea34d925
AvetisyanHayk <hayk.avetisyan@student.howest.be>
2017-05-25 16:29:20 +0200

    JUnit LinkedList, ListTestSuite

---
+++ src/be/howest/util/LinkedList.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.howest.util;
+
+/**
+ *
+ * @author Hayk
+ */
+public class LinkedList<V> implements List<V> {
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V get(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void set(int index, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ test/be/howest/util/ListTestSuite.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.howest.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Hayk
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    ArrayListTest.class,
+    LinkedListTest.class
+})
+public class ListTestSuite {
+    
+}
