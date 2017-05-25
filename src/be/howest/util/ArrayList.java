5ccbfa7700d1b4cea3b3b6bdcc2fedf50b5c882d
AvetisyanHayk <hayk.avetisyan@student.howest.be>
2017-05-25 16:08:00 +0200

    ArrayList - Beta

--- src/be/howest/util/ArrayList.java
+++ src/be/howest/util/ArrayList.java
@@ -2,53 +2,113 @@ package be.howest.util;
 
 public final class ArrayList<V> implements List<V> {
 
+    private V[] items;
+    private int size;
+    private static final int STEP = 10;
+
+    public ArrayList() {
+        clear();
+    }
+
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public int indexOf(V value) {
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < size; i++) {
+            if (value.equals(items[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public V get(int index) {
-       throw new UnsupportedOperationException();
+        return items[index];
     }
 
     @Override
     public void set(int index, V newValue) {
-        throw new UnsupportedOperationException();
+        items[index] = newValue;
     }
 
     @Override
     public void add(V value) {
-        throw new UnsupportedOperationException();
+        ensureCapacity(true);
+        items[size] = value;
+        size++;
     }
 
     @Override
     public void add(int index, V newValue) {
-        throw new UnsupportedOperationException();
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        ensureCapacity(true);
+        for (int i = size; i > index; i--) {
+            set(i, items[i - 1]);
+        }
+        set(index, newValue);
+        size++;
     }
 
     @Override
     public void remove(int index) {
-        throw new UnsupportedOperationException();
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        for (int i = index; i < size - 1; i++) {
+            if (index < size - 1) {
+                set(i, items[i + 1]);
+            } else {
+                set(i, null);
+                ensureCapacity(false);
+            }
+        }
+        size--;
     }
 
     @Override
     public boolean remove(V value) {
-        throw new UnsupportedOperationException();
+        int index = indexOf(value);
+        if (index >= 0) {
+            remove(index);
+        }
+        return index >= 0;
     }
 
     @Override
     public boolean removeAll(V value) {
-        throw new UnsupportedOperationException();
+        int index = indexOf(value);
+        boolean removed = false;
+        while (index >= 0) {
+            removed = remove(value);
+            index = indexOf(value);
+        }
+        return removed;
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException();
+        items = (V[]) new Object[STEP];
+        size = 0;
+    }
+
+    private void ensureCapacity(boolean toEncrease) {
+        if (size % STEP == 0) {
+            V[] copyArray;
+            if (toEncrease) {
+                copyArray = (V[]) new Object[size + STEP];
+            } else {
+                copyArray = (V[]) new Object[size - STEP];
+            }
+            for (int i = 0; i < items.length; i++) {
+                copyArray[i] = items[i];
+            }
+            items = copyArray;
+        }
     }
 }
