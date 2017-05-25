f66b53fd172c40ff9353573d59b066ceaa2847ca
AvetisyanHayk <hayk.avetisyan@student.howest.be>
2017-05-25 15:58:20 +0200

    JUnit ArrayList

---
+++ src/be/howest/util/ArrayList.java
@@ -0,0 +1,54 @@
+package be.howest.util;
+
+public final class ArrayList<V> implements List<V> {
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V get(int index) {
+       throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void set(int index, V newValue) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, V newValue) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+}
